3ff46707335f1584329865375f51080ba8bc3a80
scpark98 <scpark98@gmail.com>
2026-03-19 18:05:37 +0900

    초기 커밋.

---
+++ Test_CSCColorPickerDlg.h
@@ -0,0 +1,44 @@
+﻿
+// Test_CSCColorPickerDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CTestCSCColorPickerDlg 대화 상자
+class CTestCSCColorPickerDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTestCSCColorPickerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	enum TIMER_ID
+	{
+		timer_show_picker = 1,
+	};
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_CSCCOLORPICKER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnWindowPosChanged(WINDOWPOS* lpwndpos);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
